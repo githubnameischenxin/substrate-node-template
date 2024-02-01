56d77d6dcf23dc6edbaab44ed9da56d0b14a215b
chenxin <flyer.chenxin@gmail.com>
2024-02-02 02:09:44 +0800

    benchmark

--- pallets/poe/src/benchmarking.rs
+++ pallets/poe/src/benchmarking.rs
@@ -1,21 +1,44 @@
-use super::*;
+use crate::*;
 
-#[allow(unused)]
-use crate::Pallet as Poe;
-use frame_benchmarking::v1::{benchmarks, whitelisted_caller};
+use frame_benchmarking::v1::{benchmarks, whitelisted_caller, account};
 use frame_system::RawOrigin;
+use sp_std::*;
+
+fn assert_last_event<T: Config>(event: <T as Config>::RuntimeEvent) {
+	frame_system::Pallet::<T>::assert_last_event(event.into());
+}
 
 benchmarks! {
-	
-
-
-	do_something {
-		let s in 0 .. 100;
+	create_claim {
+		let d in 0 .. T::MaxClaimLength::get();
+		let claim = BoundedVec::try_from(vec![0u8; d as usize]).unwrap();
 		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
+	}: _(RawOrigin::Signed(caller.clone()), claim.clone())
 	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
+		assert_eq!(Proofs::<T>::get(&claim).is_some(), true);
+		assert_last_event::<T>(Event::ClaimCreated(caller, claim).into())
 	}
 
-	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+	revoke_claim {
+		let d in 0 .. T::MaxClaimLength::get();
+		let claim = BoundedVec::try_from(vec![0u8; d as usize]).unwrap();
+		let caller: T::AccountId = whitelisted_caller();
+		let _ = Pallet::<T>::create_claim(RawOrigin::Signed(caller.clone()).into(), claim.clone());
+	}: _(RawOrigin::Signed(caller.clone()), claim.clone())
+	verify {
+		assert_last_event::<T>(Event::ClaimRevoked(caller, claim).into())
+	}
+
+	transfer_claim {
+		let d in 0 .. T::MaxClaimLength::get();
+		let claim = BoundedVec::try_from(vec![0u8; d as usize]).unwrap();
+		let caller: T::AccountId = whitelisted_caller();
+		let target: T::AccountId = account("target", 0, 0);
+		let _ = Pallet::<T>::create_claim(RawOrigin::Signed(caller.clone()).into(), claim.clone());
+	}: _(RawOrigin::Signed(caller.clone()), claim.clone(), target.clone())
+	verify {
+		assert_last_event::<T>(Event::ClaimTransferred(caller, claim, target).into())
+	}
+
+	impl_benchmark_test_suite!(PoeModule, crate::mock::new_test_ext(), crate::mock::Test);
 }
--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
+pub use weights::WeightInfo;
 
 #[cfg(test)]
 mod mock;
@@ -8,10 +9,17 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+pub mod weights;
+
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
-    use frame_system::pallet_prelude::*;
+	pub use frame_support::pallet_prelude::*;
+    pub use frame_system::pallet_prelude::*;
+    use super::WeightInfo;
+    
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
@@ -19,10 +27,10 @@ pub mod pallet {
         #[pallet::constant]
         type MaxClaimLength: Get<u32>;
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        type WeightInfo: WeightInfo;
     }
 
     #[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
     #[pallet::storage]
@@ -56,7 +64,7 @@ pub mod pallet {
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         #[pallet::call_index(0)]
-        #[pallet::weight(0)]
+        #[pallet::weight(T::WeightInfo::create_claim(claim.len() as u32))]
         pub fn create_claim(origin: OriginFor<T>, claim: BoundedVec<u8, T::MaxClaimLength>) -> DispatchResultWithPostInfo {
             let sender = ensure_signed(origin)?;
             
@@ -73,7 +81,7 @@ pub mod pallet {
         }
 
         #[pallet::call_index(1)]
-        #[pallet::weight(0)]
+        #[pallet::weight(T::WeightInfo::revoke_claim(claim.len() as u32))]
         pub fn revoke_claim(origin: OriginFor<T>, claim: BoundedVec<u8, T::MaxClaimLength>) -> DispatchResultWithPostInfo {
             let sender = ensure_signed(origin)?;
 
@@ -89,7 +97,7 @@ pub mod pallet {
         }
 
         #[pallet::call_index(2)]
-        #[pallet::weight(0)]
+        #[pallet::weight(T::WeightInfo::transfer_claim(claim.len() as u32))]
         pub fn transfer_claim(origin: OriginFor<T>, claim: BoundedVec<u8, T::MaxClaimLength>, target: T::AccountId) -> DispatchResultWithPostInfo {
             let sender = ensure_signed(origin)?;
 
